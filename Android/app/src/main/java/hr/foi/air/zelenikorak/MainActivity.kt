d7374bc45f0dbf7b215bb4cbe2fcc5edb2e063c2
dfabijani22 <dora.fabijanic@foi.hr>
2023-11-01 18:59:04 +0100

    Postavljanje firebase-a

--- Android/app/src/main/java/hr/foi/air/zelenikorak/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/zelenikorak/MainActivity.kt
@@ -2,10 +2,17 @@ package hr.foi.air.zelenikorak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //val database = Firebase.database
+        //val myRef = database.getReference("message")
+
+        //myRef.setValue("Hello, World!")
     }
+
 }
