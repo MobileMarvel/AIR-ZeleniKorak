cf345a81dbd612baf27fac76e12a9c42cabeba70
dfabijani22 <dora.fabijanic@foi.hr>
2023-11-03 15:27:18 +0100

    Google prijava  -dodane ikone za prijavu preko googlea i facebooka  -omogucena prijava s google accountom

--- Android/app/src/main/java/hr/foi/air/zelenikorak/LoginActivity.kt
+++ Android/app/src/main/java/hr/foi/air/zelenikorak/LoginActivity.kt
@@ -1,17 +1,26 @@
 package hr.foi.air.zelenikorak
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 import hr.foi.air.zelenikorak.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
     private lateinit var auth: FirebaseAuth;
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,17 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        binding.btnGoogleSignIn.setOnClickListener {
+            signInGoogle()
+        }
 
         binding.loginButton.setOnClickListener{
             val username = binding.username.text.toString()
@@ -29,6 +49,7 @@ class LoginActivity : AppCompatActivity() {
                     if(it.isSuccessful){
                         Toast.makeText(this, "Uspješna prijava", Toast.LENGTH_SHORT).show()
                         val intent = Intent(this, MainActivity::class.java)
+                        intent.putExtra("email", username)
                         startActivity(intent)
                     }else{
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
@@ -37,9 +58,41 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
 
-    override fun onStart() {
-        super.onStart()
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        val extras = result.data?.extras
+        if(result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
 
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUIGoogle(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun updateUIGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this , MainActivity::class.java)
+                intent.putExtra("email" , account.email)
+                intent.putExtra("name" , account.displayName)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- Android/app/src/main/java/hr/foi/air/zelenikorak/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/zelenikorak/MainActivity.kt
@@ -1,18 +1,33 @@
 package hr.foi.air.zelenikorak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import hr.foi.air.zelenikorak.databinding.ActivityLoginBinding
+import hr.foi.air.zelenikorak.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //val database = Firebase.database
-        //val myRef = database.getReference("message")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //myRef.setValue("Hello, World!")
+        auth = FirebaseAuth.getInstance()
+
+        val email = intent.getStringExtra("email")
+        val displayName = intent.getStringExtra("name")
+
+        binding.textView.text = email + "\n" + displayName
+
+        binding.btnSignOut.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 
 }
